5e4cd2c17423f7f36a3249942d3d523646025b14
ShawnM821 <61438996+ShawnM821@users.noreply.github.com>
2021-09-30 22:47:40 -0500

    Changed date to date data type

--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_183920) do
     t.string "title"
     t.string "author"
     t.float "price"
-    t.string "date"
+    t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
